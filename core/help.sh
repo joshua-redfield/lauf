4572302db602ff0c341b9600ef454653ec1b8f50
joshua-redfield <joshua.redfield@gmail.com>
2011-01-10 01:07:26 -0500

    Apparently I broke the plugins arguement when cleaning stuff up in core/help.sh, Fixed and working now.

--- core/help.sh
+++ core/help.sh
@@ -11,15 +11,13 @@ if [ ${lauf_exec2:=unset} = "with" ] && [ ! ${lauf_exec3:=unset} = "plugin" ]; t
     elif [ ! -z "$test2" ]; then
         info ${manual} | zenity $lauf_app_options --text-info --width=$(($lauf_height+200)) --height=${lauf_height}
     fi
-    fi
     return
-fi
-if [ ${lauf_exec2:=unset} = "with" ] && [ ${lauf_exec3:=unset} = "plugin" ] && [ ! ${lauf_exec4:=unset} = "unset" ]; then
-        if [ -e $lauf_plugin_dir/$lauf_exec4'.sh' ]; then
+elif [ ${lauf_exec2:=unset} = "with" ] && [ ${lauf_exec3:=unset} = "plugin" ]; then
+    if [ -e $lauf_plugin_dir/$lauf_exec4'.sh' ]; then
         usage=$(head -n7 $lauf_plugin_dir/$lauf_exec4'.sh' | tail -n1 | tr -d '#')
         lauf_notify "${lauf_exec4:=unset}'s Usage:" "$usage"
-        fi
-        return
+    fi
+    return
 fi
 zenity $lauf_app_options --text-info --width=$(($lauf_height+200)) --height=${lauf_height} --title="${lauf_app_name}" --filename="${lauf_app_dir}/README.txt"
 if [ $? = 0 ]; then
